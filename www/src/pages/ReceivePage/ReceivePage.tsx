7917889f8917debe4bb4475589da489a2e7d2d8a
Rostyslav-Tupis <rostyslavichh@gmail.com>
2024-04-29 01:04:52 +0300

    fix infinit scroll

--- www/src/pages/ReceivePage/ReceivePage.tsx
+++ www/src/pages/ReceivePage/ReceivePage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import moment from 'moment'
 import { PostType, usePostConnectionLazyQuery } from '@/__generated__/graphql'
+import { PostCard } from './components/post-card'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { PostCard } from '../app/components/Post-card'
 
 export const ReceivePage = () => {
@@ -10,10 +12,11 @@ export const ReceivePage = () => {
     )
     const [gerMorePosts, { data }] = usePostConnectionLazyQuery({
         variables: {
-            limit: 100,
+            limit: 3,
         },
     })
     const [posts, setPosts] = useState<PostType[]>([])
+    const [page, setPage] = useState<number | null | undefined>(0)
 
     useEffect(() => {
         gerMorePosts()
@@ -22,20 +25,32 @@ export const ReceivePage = () => {
     useEffect(() => {
         if (data) {
             setPosts(prev => [...prev, ...data.postConnection.nodes])
+            setPage(data.postConnection.pageInfo.nextPageCursor)
         }
     }, [data])
 
+    const fetchNextPage = () => {
+        gerMorePosts({
+            variables: {
+                nextPageCursor: page,
+            },
+        })
+    }
+
     return (
         <div className="container mb-12 flex flex-col items-center pb-8	pl-8 pr-8 pt-8">
-            {data &&
-                posts.map(post => (
-                    <PostCard
-                        title={post.title}
-                        disc={post.content}
-                        date={moment(post.createdAt).format('L')}
-                        imageUrl={post.imageUrl}
-                    />
-                ))}
+            <InfiniteScroll dataLength={posts.length} next={fetchNextPage} hasMore={true} loader={<h4>Loading...</h4>}>
+                {data &&
+                    posts.map(post => (
+                        <PostCard
+                            key={post.id}
+                            title={post.title}
+                            disc={post.content}
+                            date={moment(post.createdAt).format('L')}
+                            imageUrl={post.imageUrl}
+                        />
+                    ))}
+            </InfiniteScroll>
         </div>
     )
 }
