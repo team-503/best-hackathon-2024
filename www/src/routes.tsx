79b2178ea3bb14d62f1c3b8de9b84645ce65ac4f
Oleshkooo <olehchoma@gmail.com>
2024-04-29 03:08:38 +0300

    Fix routes issue

--- www/src/routes.tsx
+++ www/src/routes.tsx
@@ -6,6 +6,8 @@ import { CreatePostPage } from '@/pages/app/create-post.page'
 import { PostPage } from '@/pages/app/post-page'
 import { ProfileIdPage } from '@/pages/app/profile-id.page'
 import { ProfileMePage } from '@/pages/app/profile-me'
+import { ProvidePage } from '@/pages/app/provide.page'
+import { ReceivePage } from '@/pages/app/receive.page'
 import { LoginPage } from '@/pages/auth/login-page/login.page'
 import { RegisterChoicePage } from '@/pages/auth/register-page/register-choice.page'
 import { RegisterProvidePage } from '@/pages/auth/register-page/register-provide.page'
@@ -16,8 +18,6 @@ import { MainPage } from '@/pages/main.page'
 import { NotFoundPage } from '@/pages/not-found.page'
 import { memo } from 'react'
 import { useRoutes } from 'react-router-dom'
-import { ReceivePage } from './pages/app/receive-page'
-import { ProvidePage } from '@/pages/app/provide.page'
 
 export interface RoutesProps {}
 export const Routes: React.FC<RoutesProps> = memo(() => {
